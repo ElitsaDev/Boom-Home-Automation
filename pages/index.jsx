626ec74f803c3b04ea8164e3ee339f7d7cfa1164
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-13 13:14:58 +0200

    added weather component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,16 @@ import Example from "../src/components/example/Example";
 import Login from "../src/components/login/Login";
 import LoginPage from "../src/components/loginPage/LoginPage"
 import User from "../src/components/user/User";
+import Weather from "../src/components/weather/Weather";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function Index() {
 
   return (
-  <User />
+  <Weather />
   );
 }
---
+++ src/components/weather/Weather.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Container, Grid, Typography } from "@mui/material";
+import styles from "./Weather.module.scss";
+import classNames from "classnames";
+
+export default function Weather({
+    degrees,
+    type,
+}) {
+    return (
+        <div className={classNames(styles["weather-wrapper"])}>
+            <Container maxWidth="xl">
+                <Grid container>
+                    <Grid item xs={6}>
+                        <img src={`images/${type}` +".svg"}
+                            alt="weather image"
+                            className={styles.image}
+                    />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography className={styles.header} >
+                            Weather
+                        </Typography>
+                        <Typography className={styles.degrees} >
+                            {degrees}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
