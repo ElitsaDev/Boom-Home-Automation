328ffb7baf50e925bb4f8c661ba54f02886bbd88
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-12 22:17:22 +0300

    card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,9 +3,10 @@ import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
 
-export default function ActionAreaCard({ iconUrl, outlined }) {
+export default function CardComponent({ iconUrl, outlined = false, onClick }) {
+
     return (
-        <Card className={classNames(styles.card, outlined == true && styles.border)} >
+        <Card className={classNames(styles.card, outlined == true && styles.border)} onClick={onClick}>
             <CardActionArea>
                 <CardContent>
                     {iconUrl !== '' &&
