84d7d90d580791285c342d6027f229c8a2a28235
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-13 12:47:39 +0300

    added div root element

--- src/components/scenes/Scenes.jsx
+++ src/components/scenes/Scenes.jsx
@@ -4,20 +4,22 @@ import classNames from "classnames";
 import { Grid } from "@mui/material";
 import CardComponent from "../card/Card";
 
-export default function Scenes({cards, selected}){
+export default function Scenes({ cards, selected }) {
 
     const onClick = () => {
         selected = true;
     }
 
     return (
-        <Grid container spacing={35} className={classNames(styles["scenes-container"])}>
-            {cards.map(card =>{
-            <Grid key={card.iconUrl} item xs={4}>
-                    {<CardComponent iconUrl={card.iconUrl} outlined={card.outlined} onClick={onClick} />}
-            </Grid> 
-            })}
-            
-        </Grid>
+        <div className={classNames(styles["scenes-container"])}>
+            <Grid container spacing={35} >
+                {cards.map(card => {
+                    <Grid item xs={4} key={card.iconUrl} >
+                        {<CardComponent iconUrl={card.iconUrl} outlined={card.outlined} onClick={onClick} />}
+                    </Grid>
+                })}
+
+            </Grid>
+        </div>
     );
 }
