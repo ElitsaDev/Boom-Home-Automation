b98a192b8769e9d6e73c917c0e862f5bce73fb0c
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-15 23:48:40 +0300

    added devices

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,24 +1,32 @@
 
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Chip, Badge } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
 
-export default function CardComponent({ iconUrl, outlined = false, onClick, title }) {
-    
+export default function CardComponent({ iconUrl, outlined = false, onClick, title, variant }) {
+
     return (
         <Card className={classNames(styles.card, outlined == true && styles.border)} onClick={onClick}>
             <CardActionArea>
-                <CardContent> 
-                    {!!iconUrl  &&
+                <CardContent>
+                    {!!iconUrl &&
                         <CardMedia
                             sx={{ width: 120, height: 100, margin: 'auto' }}
                             image={iconUrl}
                             alt="card image"
-                        /> 
+                        />
                     }
-                   {title &&
-                    <Typography className={styles.text} sx={{ mb: 1.0 }} color="#7441F3" variant="h5" component="div">{title}</Typography>
+                    {title &&
+                        <Typography className={styles.text} sx={{ mb: 1.0 }} color="#7441F3" variant="h5" component="div">{title}</Typography>
                     }
+                    {title == "offline" 
+                    && 
+                    <Chip variant="filled"
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        label="!" />}
                 </CardContent>
             </CardActionArea>
         </Card>
---
+++ src/components/devices/Devices.jsx
@@ -0,0 +1,40 @@
+
+import CardComponent from "../card/Card";
+import styles from "./Devices.module.scss";
+
+import { Grid, Paper } from "@mui/material";
+
+export default function Devices({ devices = [
+    {
+    "title": String,
+    "iconUrl": String
+   }
+]}) {
+
+    const variant = {
+        on: 'on',
+        off: 'off',
+        offline: 'offline'
+    };
+    console.log(devices[0])
+    return (
+        <div className={styles['devices-wrapper']}>
+            <Grid container gap={1}>
+                    {devices[0].map(device => (
+                        <Grid item xs={4} key={device.id} className={styles.item} >
+                            {(device.id == 1) &&
+                                <CardComponent  iconUrl={device.iconUrl} title={'on'} variant={variant.on} />
+                            }
+                            {(device.id == (devices.length)) &&
+                                <CardComponent  iconUrl={device.iconUrl} title={'offline'} variant={variant.offline} />
+                            }
+                            {(device.id != 1 && device.id != devices.length) &&
+                                <CardComponent  iconUrl={device.iconUrl} title={'off'} variant={variant.off} />
+                            }
+
+                       </Grid>
+                    ))}
+            </Grid>
+        </div>
+    );
+}
