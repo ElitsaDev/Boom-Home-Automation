0ed0460874bd735b61a5e70b825f56f52404d8e8
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-16 14:02:15 +0300

    remove room

--- pages/room/index.jsx
+++
@@ -1,8 +0,0 @@
-import Devices from "../../src/components/devices/Devices";
-import devices from '../../data/devices.json';
-
-export default function Room() {
-    return (
-        <Devices devices={Object.values(devices)}/>
-    );
-}
