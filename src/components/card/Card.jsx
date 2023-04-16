b36b69f95d514251d0dd1a61a03dd7d2d67871ef
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-16 18:33:50 +0300

    changed card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,37 +1,42 @@
 
-import { Card, CardActionArea, CardContent, CardMedia, Typography, Chip, Badge } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Chip, Badge, CardHeader, Stack } from "@mui/material";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
-export default function CardComponent({ iconUrl, outlined = false, onClick, title, variant = {
-        on: 'on',
-        off: 'off',
-        offline: 'offline'
-    }
-}) {
-    
+export default function CardComponent({ iconUrl, outlined = false, onClick, title, variant }) {
+
+
+
     return (
         <Card className={classNames(styles.card, outlined == true && styles.border, variant == "offline" && styles.colored)} onClick={onClick}>
             <CardActionArea>
                 {(title == "offline") &&
-                    <Chip className={classNames(styles.status)}
-                    label={<PriorityHighIcon style={{color: "#EB675C"}} />}
-                    
-                />}
-                <CardContent>
-                    {!!iconUrl &&
-                        <CardMedia
-                            sx={{ width: 120, height: 100, margin: 'auto' }}
-                            image={iconUrl}
-                            alt="card image"
-                        />
-                    }
-                    {title &&
+
+                    <Stack spacing={1}>
+                        <Chip label='!' variant="filled" />
+                    </Stack>
+                }
+
+                {title != '' ?
+                    (<CardContent>
+                        {!!iconUrl &&
+                            <CardMedia
+                                sx={{ width: 120, height: 100, margin: 'auto' }}
+                                image={iconUrl}
+                                alt="card image"
+                            />}
                         <Typography className={styles.text} sx={{ mb: 1.0 }} color="#7441F3" variant="h5" component="div">{title}</Typography>
-                    }
-                    
-                </CardContent>
-                
+                    </CardContent>)
+                    :
+                    (<CardContent>
+                        {!!iconUrl &&
+                            <CardMedia
+                                sx={{ width: 120, height: 100, margin: 'auto' }}
+                                image={iconUrl}
+                                alt="card image"
+                            />}
+                    </CardContent>)
+                }
             </CardActionArea>
         </Card>
     );
