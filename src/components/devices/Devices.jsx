5fad8e15519dae8ea4a7d8e3199cd67ae12f3e8a
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-04-16 16:20:06 +0300

    add variant

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,32 +1,23 @@
-
+import { Grid } from "@mui/material";
 import CardComponent from "../card/Card";
 import styles from "./Devices.module.scss";
 
-import { Grid, Paper } from "@mui/material";
 
-export default function Devices({ devices }) {
 
-    const variant = {
-        on: 'on',
-        off: 'off',
-        offline: 'offline'
-    };
+export default function Devices({ devices = [{
+    title: '',
+    iconUrl: '',
+   } ]
+}) {
+
+    
 
     return (
         <div className={styles['devices-wrapper']}>
             <Grid container gap={1}>
                 {Array.from(devices[0]).map(device => (
                     <Grid item xs={4} key={device.id} className={styles.item} >
-                        {(device.id == 1) &&
-                            <CardComponent iconUrl={device.iconUrl} title={'on'} variant={variant.on} />
-                        }
-                        {(device.id == (devices[0].length)) &&
-                            <CardComponent iconUrl={device.iconUrl} title={'offline'} variant={variant.offline} />
-                        }
-                        {(device.id > 1 && device.id < (devices[0].length)) &&
-                            <CardComponent iconUrl={device.iconUrl} title={'off'} variant={variant.off} />
-                        }
-
+                        <CardComponent iconUrl={device.iconUrl} title={device.title} variant={variant} />
                     </Grid>
                 ))}
 
@@ -34,3 +25,7 @@ export default function Devices({ devices }) {
         </div>
     );
 }
+
+/*
+
+*/
